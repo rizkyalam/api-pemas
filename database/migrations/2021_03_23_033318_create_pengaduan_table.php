038612bf5be465f1349dd8214506a79d1a085710
rizkyalam <riz.alams09@gmail.com>
2021-03-23 15:17:33 +0700

    initial commit

---
+++ database/migrations/2021_03_23_033318_create_pengaduan_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePengaduanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pengaduan', function (Blueprint $table) {
+            $table->increments('id_pengaduan');
+            $table->date('tgl_pengaduan');
+            $table->char('nik', 16);
+            $table->text('isi_laporan');
+            $table->string('foto');
+            $table->enum('status', ['0', 'proses', 'selesai']);
+            $table->timestamps();
+
+            // relationship with masyarakat table
+            $table->foreign('nik')
+                  ->references('nik')
+                  ->on('masyarakat')
+                  ->onUpdate('cascade')
+                  ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pengaduan');
+    }
+}
---
+++ database/migrations/2021_03_23_033347_create_tanggapan_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTanggapanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tanggapan', function (Blueprint $table) {
+            $table->increments('id_tanggapan');
+            $table->integer('id_pengaduan')->unsigned();
+            $table->date('tgl_tanggapan');
+            $table->text('tanggapan');
+            $table->integer('id_petugas');
+            $table->timestamps();
+
+            // relationship with pengaduan table
+            $table->foreign('id_pengaduan')
+                  ->references('id_pengaduan')
+                  ->on('pengaduan')
+                  ->onUpdate('cascade')
+                  ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tanggapan');
+    }
+}
